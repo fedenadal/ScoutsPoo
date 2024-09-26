1bdf50876d956c2c87c4acd80773bf51610311b1
ArnouxDen <ArnouxDen@gmail.com>
2024-09-26 20:59:42 -0300

    PrimerCommit

---
+++ src/main/java/com/scoutspoo/scoutspoo/ScoutspooApplication.java
@@ -0,0 +1,13 @@
+package com.scoutspoo.scoutspoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutspooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutspooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoutspoo/scoutspoo/ScoutspooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoutspoo.scoutspoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutspooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
